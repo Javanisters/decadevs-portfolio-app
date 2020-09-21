d1395317dea5535b1be943d5b3755f4972e18d12
Omojolade <omolade.omololu@decagon.dev>
2020-09-21 22:25:15 +0100

    Create Project Model. closes #4

--- src/main/java/com/javanisters/portfolioapp/models/Project.java
+++ src/main/java/com/javanisters/portfolioapp/models/Project.java
@@ -1,4 +1,14 @@
 package com.javanisters.portfolioapp.models;
 
+import lombok.Data;
+
+@Data
 public class Project {
+    private int id;
+    private String summary;
+    private String details;
+    private String github_link;
+    private String site_link;
+    private String tools_used;
+
 }

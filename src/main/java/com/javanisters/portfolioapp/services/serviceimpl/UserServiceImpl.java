dfcd591b26e6aae598412430d1fa7698eca3c8b0
emysilva <emysly12@gmail.com>
2020-09-22 01:41:10 +0100

    create user service closes #2

--- src/main/java/com/javanisters/portfolioapp/services/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/javanisters/portfolioapp/services/serviceimpl/UserServiceImpl.java
@@ -1,4 +1,45 @@
 package com.javanisters.portfolioapp.services.serviceimpl;
 
-public class UserServiceImpl {
+import com.javanisters.portfolioapp.models.User;
+import com.javanisters.portfolioapp.repositories.UserRepository;
+import com.javanisters.portfolioapp.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserDetailsService, UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        final Optional<User> optionalUser = userRepository.findByEmail(email);
+
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        }
+        else {
+            throw new UsernameNotFoundException(MessageFormat.format("User with email {0} cannot be found.", email));
+        }
+    }
+
+    public void signUpUser(User user) {
+
+        final String encryptedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+
+        user.setPassword(encryptedPassword);
+
+        userRepository.save(user);
+
+    }
 }
